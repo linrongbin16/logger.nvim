f2a945f32cef6641d34861e1f11536d01a103985
linrongbin16 <linrongbin16@outlook.com>
2023-03-14 22:12:22 +0800

    update

--- lua/logger.lua
+++ lua/logger.lua
@@ -5,33 +5,53 @@ local EchoHl = {
     ["DEBUG"] = "Comment",
 }
 local Defaults = {
-    log_level = "INFO",
-    console_log = true,
-    name = "logger",
-    file_log = false,
-    file_log_name = nil,
-    file_log_dir = vim.fn.stdpath("data"),
-    file_log_path = nil,
+    level = "INFO",
+    console = true,
+    name = nil,
+    file = false,
+    file_name = nil,
+    file_dir = vim.fn.stdpath("data"),
+    file_path = nil,
 }
 local Config = {}
 
 local function setup(option)
     Config = vim.tbl_deep_extend("force", vim.deepcopy(Defaults), option or {})
-    if Config.file_log_name and string.len(Config.file_log_name) > 0 then
+    if Config.file_name and string.len(Config.file_name) > 0 then
         -- For Windows: $env:USERPROFILE\AppData\Local\nvim-data\lsp-progress.log
         -- For *NIX: ~/.local/share/nvim/lsp-progress.log
-        Config.file_log_path =
-            string.format("%s/%s", Config.file_log_dir, Config.file_log_name)
+        Config.file_path =
+            string.format("%s/%s", Config.file_dir, Config.file_name)
+    end
+    assert(type(Config.name) == "string" and string.len(Config.name) > 0)
+    assert(
+        type(Config.level) == "string"
+            and (
+                Config.level == "ERROR"
+                or Config.level == "WARN"
+                or Config.level == "INFO"
+                or Config.level == "DEBUG"
+            )
+    )
+    if Config.file then
+        assert(
+            type(Config.file_name) == "string"
+                and string.len(Config.file_name) > 0
+        )
+        assert(
+            type(Config.file_dir) == "string"
+                and string.len(Config.file_dir) > 0
+        )
     end
 end
 
 local function log(level, msg)
-    if vim.log.levels[level] < vim.log.levels[Config.log_level] then
+    if vim.log.levels[level] < vim.log.levels[Config.level] then
         return
     end
 
     local msg_lines = vim.split(msg, "\n")
-    if Config.console_log then
+    if Config.console then
         vim.cmd("echohl " .. EchoHl[level])
         for _, line in ipairs(msg_lines) do
             vim.cmd(
@@ -46,8 +66,8 @@ local function log(level, msg)
         end
         vim.cmd("echohl None")
     end
-    if Config.file_log then
-        local fp = io.open(Config.file_log_path, "a")
+    if Config.file then
+        local fp = io.open(Config.file_path, "a")
         if fp then
             for _, line in ipairs(msg_lines) do
                 fp:write(
